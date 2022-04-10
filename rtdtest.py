089cb3e1f95a82da579cb4f5d57ad1fb39585900
Daniel Valdelamar <71929193+juniortec81@users.noreply.github.com>
2022-04-09 21:19:11 -0500

    Delete rtdtest.py

--- rtdtest.py
+++
@@ -1,9 +0,0 @@
-import librtd
-import time
-
-while True:
-    test = librtd.get(0,1)
-    testres = librtd.getRes(0,1)
-    Fer = (test*9/5)+32
-    print('Tempemperature C {} Resistance {}'.format('%.4f' % test,'%.4f' % testres))
-    time.sleep(1) 
